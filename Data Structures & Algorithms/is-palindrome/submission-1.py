d211d471386d699fa9c0683772ac2ca1299df6ca
Jimmy Frosting <jimmyfrosting@gmail.com>
2026-08-27 07:45:03 +0100

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean = ""
+
+        for c in s:
+            if c.isalnum():
+                clean += c.lower()
+
+        return clean == clean[::-1]
+
